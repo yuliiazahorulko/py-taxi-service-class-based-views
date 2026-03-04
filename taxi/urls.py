72acc5fcc623f9d4e58c4a95d32ed329b169dd86
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-03-04 13:29:08 +0200

    covered with code

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from .views import index
+from .views import index, \
+    ManufacturerListView, \
+    CarListView, \
+    CarDetailView, \
+    DriverListView, \
+    DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list-view"
+    ),
+    path("cars/", CarListView.as_view(), name="car-list-view"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail-view"),
+    path("drivers/", DriverListView.as_view(), name="driver-list-view"),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail-view"
+    ),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,37 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturers.html"
+    context_object_name = "manufacturer_list"
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    template_name = "taxi/car_list.html"
+    context_object_name = "car_list"
+    queryset = Car.objects.all()
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    queryset = Car.objects.all()
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    template_name = "taxi/driver_list.html"
+    context_object_name = "driver_list"
+    queryset = Driver.objects.all()
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.all()
